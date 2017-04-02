1ee1f140e8d73ace3b980bd1648df97190cf4819
Ial Jonas <Ial Jonas@192.168.1.9>
2017-04-02 17:25:31 -0300

    Criacao da classe ClienteTeste

---
+++ src/ClienteTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClienteTest {
+	
+	private Cliente cl;
+
+	@Before
+	public void setUp() throws Exception {
+		Cliente cl=new Cliente("Ial", "01531075029", 27, "M");
+	}
+
+	@Test
+	public void testCadastrarCliente() {
+		
+	
+	}
+
+}
